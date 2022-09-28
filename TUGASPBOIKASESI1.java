7409512662f0d78362cc2a7d9b6aa3c00a01f2b0
ikasalbella <114545046+ikasalbella@users.noreply.github.com>
2022-09-28 08:50:20 +0700

    Add files via upload

---
+++ TUGASPBOIKASESI1.java
@@ -0,0 +1,14 @@
+public class TUGASPBOIKASESI1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Belajar JAVA");
+        System.out.println("Sangat mudah sekali");
+        System.out.println("dan sangat menyenangkan");
+    } 
+}
+    
+    
+
